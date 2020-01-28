8c11d06106538ffe0c154496e385386fbf327070
Carlos Alva <alvacarlosit@gmail.com>
2020-01-28 13:57:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-console.error("I\'m in grave danger! Send HELP NOW")
-console.log("Keep going")
-console.warn('Be careful there!')
+console.error("I\'m in grave danger! Send HELP NOW") //error message
+console.log("I would be in a bad situation") //describe event
+console.warn('Be careful there!') //advise user of event
